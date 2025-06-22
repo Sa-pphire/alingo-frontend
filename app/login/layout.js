a6f49e61d2cc25dd73ff77e2f66151914322e5f1
sapphire <chidinmaakunwa@gmail.com>
2025-06-22 21:11:49 +0100

    CSS Fix

--- app/login/layout.js
+++ app/login/layout.js
@@ -9,9 +9,11 @@ export default function SigninLayout({ children }) {
     return (
         <div className='items-center justify-center bg-cover bg-center overflow-hidden'>
             {/* Mobile Title Outside Card */}
-            <h1 className="text-3xl font-bold text-white text-center mt-12 mb-4 sm:hidden"> Sign In </h1>
-            <p className="text-center text-sm text-white-600 mb-4 sm:hidden">Let us get to know you more</p>
-            <div className="flex items-center relative justify-center sm:px-4 sm:min-h-screen">
+            <div className="bg-origin-border bg-contain bg-center text-white sm:hidden">
+                <h1 className="text-3xl font-bold text-white text-center mt-12 mb-4 sm:hidden"> Sign In </h1>
+                <p className="text-center text-sm text-white-600 mb-4 sm:hidden">Let us get to know you more</p>
+            </div>
+            <div className="flex items-center bg-white lg:bg-[url('/images/bg.jpg')] relative justify-center sm:px-4 sm:min-h-screen">
                 <div className="bg-white shadow-lg w-full max-w-md p-10 sm:mb-[-15px] pb-[100px] sm:mt-0 sm:mb-0 sm:pb-8 sm:rounded-lg">
                     {children}
                 </div>
