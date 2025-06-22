d366c9267d80842a1830154283f0411d566be7b0
sapphire <chidinmaakunwa@gmail.com>
2025-06-22 21:22:55 +0100

    CSS Fix

--- app/login/layout.js
+++ app/login/layout.js
@@ -12,7 +12,7 @@ export default function SigninLayout({ children }) {
             <h1 className="text-3xl font-bold text-white text-center mt-12 mb-4 sm:hidden"> Sign In </h1>
             <p className="text-center text-sm text-white-600 mb-4 sm:hidden">Let us get to know you more</p>
             <div className="flex items-center relative justify-center sm:px-4 sm:min-h-screen">
-                <div className="bg-white shadow-lg w-full max-w-md p-6 sm:mb-[-15px] pb-[100px] sm:mt-0 sm:mb-0 sm:pb-8 sm:rounded-lg">
+                <div className="bg-white shadow-lg w-full max-w-md p-6 sm:mb-[-15px] py-[100px] sm:mt-0 sm:mb-0 sm:pb-8 sm:rounded-lg">
                     {children}
                 </div>
                 <CharacterIllustration />
